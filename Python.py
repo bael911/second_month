8b7b374a941fbddc95b7a5bb5dcea1dfa1d24981
bael911 <baelalymbaev154@gmail.com>
2022-03-04 15:20:05 +0300

    hw1

---
+++ Python.py
@@ -0,0 +1,11 @@
+name = 'bael'
+result = input('Введите имя :')
+while True:
+    if name != result:
+        print('НЕ верно')
+    elif name == result:
+        print('Молодец')
+        break
+    else:
+        print("Сага жарашпайт")
+        break
---
+++ lesson1.16.py
@@ -0,0 +1,86 @@
+class Dog:
+    def __init__(self, weight, color, height, voiceText):
+        self.weight = weight
+        self.color = color
+        self.height = height
+        self.voiceText = voiceText
+
+    def voice(self):
+        print(f'voice: {self.voiceText}')
+
+    def __str__(self):
+        return f'weight: {self.weight}\n' \
+               f'color: {self.color}\n' \
+               f'height: {self.height}'
+
+
+rex = Dog(74, 1.75, 'black', 'gaf gaf')
+print('эТО СОБАКА')
+print(rex)
+rex.voice()
+print("-" * 40)
+
+
+class Cat:
+    def __init__(self, weight, color, height, voiceText):
+        self.weight = weight
+        self.color = color
+        self.height = height
+        self.voiceText = voiceText
+
+    def voice(self):
+        print(f'voice: {self.voiceText}')
+
+    def __str__(self):
+        return f'weight: {self.weight}\n' \
+               f'color: {self.color}\n' \
+               f'height: {self.height}'
+
+
+barsik = Cat(20, 1.05, 'black and white', 'mur mur')
+print('Это кот')
+print(barsik)
+barsik.voice()
+print("-" * 40)
+
+
+class Cow:
+    def __init__(self, weight, color, height, voiceText):
+        self.weight = weight
+        self.color = color
+        self.height = height
+        self.voiseText = voiceText
+
+    def voise(self):
+        print(f'{self.voiseText}')
+
+    def __str__(self):
+        return f'weight: {self.weight}\n' \
+               f'color: {self.color}\n' \
+               f'height: {self.height}'
+
+Ashab_Tamaev = Cow(180,1.80, 'black-white', 'muuuuuuuuuuuuuuuuuuuu muuuuuuuuuuuuuuuuuuuuu')
+print('Это Корова')
+print(Ashab_Tamaev)
+Ashab_Tamaev.voise()
+print("-" * 40)
+
+class Bear:
+    def __init__(self, weight, color, height, voiceText):
+        self.weight = weight
+        self.color = color
+        self.height = height
+        self.voiseText = voiceText
+
+    def voise(self):
+        print(f'{self.voiseText}')
+
+    def __str__(self):
+        return f'weight: {self.weight}\n' \
+               f'color: {self.color}\n' \
+               f'height: {self.height}'
+
+Vasiliy = Bear(201,1.99, 'brown', 'sjfhsjbcauydgakugakbaj')
+print('Это Медведь')
+print(Vasiliy)
+Vasiliy.voise()
